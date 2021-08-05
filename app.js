e5ea6c52e1bbd9c6946c1501c9bb0c3dd409e90a
Surya Pratap Singh <suryaprataps424@gmail.com>
2021-08-05 15:50:10 +0530

    initial commit

--- app.js
+++ app.js
@@ -16,21 +16,43 @@ var southWest = L.latLng(-89.98155760646617, -180),
 var bounds = L.latLngBounds(southWest, northEast);
 
 myMap.setMaxBounds(bounds);
-document.getElementById('submit').addEventListener('click',()=>{
-  var api ='https://io.adafruit.com/api/v2/anurag004/feeds/{device}/data/retain?X-AIO-KEY=aio_yVFu33153a7DvzJ13IJmfbJvsHea'
-  let device = document.getElementById('device').value
-  api=api.replace('{device}',device)
-  console.log(device)
+document.getElementById("submit").addEventListener("click", () => {
+  var api =''
+  let device = document.getElementById("device").value;
+  api = api.replace("{device}", device);
+  console.log(device);
   fetch(api)
-  .then(response => response.text())
-  .then(x => {
-    x = x.slice(11,-6)
-    x = x.replace(/'/g,'"')
-    x = JSON.parse(x)
-    document.getElementById('pm25').innerText=x.PM25
-    document.getElementById('co').innerText=x.CO
-    document.getElementById('co2').innerText=x.CO2
-    document.getElementById('o3').innerText=x.O3
-    console.log(x)
-  }).catch(e=>alert('device not found'))
-})
+    .then((response) => response.text())
+    .then((x) => {
+      x = x.slice(11, -6);
+      x = x.replace(/'/g, '"');
+      x = JSON.parse(x);
+      document.getElementById("pm25").innerText = x.PM25;
+      document.getElementById("co").innerText = x.CO;
+      document.getElementById("co2").innerText = x.CO2;
+      document.getElementById("o3").innerText = x.O3;
+      console.log(x);
+    })
+    .catch((e) => alert("device not found"));
+});
+var e = document.getElementsByClassName("item");
+var m = document.getElementsByClassName("message");
+m[0].style.display ='block'
+e[0].id = "active";
+for (let i = 0; i < 4; i++) {
+  e[i].addEventListener("click", () => {
+    for (let j = 0; j < 4; j++) {
+      if(e[i].innerText==m[j].id){
+        m[j].style.display = 'block'
+        console.log(e[i].innerText);
+      }else{
+        m[j].style.display ='none'
+      }
+    }
+    let x = document.getElementById("active");
+    if (x) {
+      x.id = "";
+    }
+    e[i].id = "active";
+  });
+}
